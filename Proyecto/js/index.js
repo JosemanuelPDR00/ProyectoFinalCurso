6dea6132622faf6c069b0eef7fdff3f1c56ccdd1
JosemaPerez00 <77966853+JosemanuelPDR00@users.noreply.github.com>
2022-05-20 23:42:51 +0200

    Terminar el cuerpo humano
    Falta añadir la funcionalidad del cuerpo humano para que salgan solo los ejercicios que hemos pulsado

--- Proyecto/js/index.js
+++ Proyecto/js/index.js
@@ -29,8 +29,8 @@ function expReg(){
     var contra = document.getElementById("contra").value;
 
 
-    let expresionRegularUsuario = /^([A-Z][a-zA-Z]{5,}|[A-Z][a-zA-Z]{5,}[0-9]{1,})$/ //1 mayuscula inicial, seguido de 5 letras, y puede seguirle un numero o mas.
-    let expresionRegularContra = /^[A-Z][a-zA-Z]{5,}[0-9]{1,}.{0,}$/ //1 mayuscula inicial, minimo 5 letras mas, minimo un numero y opcional caracteres raros al final.
+    let expresionRegularUsuario = /^[\w]{3,}$/ //El nombre del usuario, se compone caracteres alfanumericos con un minimo de 3 caraxteres.
+    let expresionRegularContra = /^(?=.{0,}[0-9]).{0,}(?=.{0,}[A-Z]).{0,}(?=.{0,}[a-z]).{0,}(?=.{0,}[!?=/&%$#@.]).{0,}$/ //1 mayuscula inicial, minimo 5 letras mas, minimo un numero y opcional caracteres raros al final.
 
 
     if(expresionRegularUsuario.test(usuario) && expresionRegularContra.test(contra)){
@@ -46,7 +46,6 @@ function expReg(){
 
     }
 }
-
 
 // CRONOMETRO ---------------------------------
 
@@ -87,4 +86,10 @@ function formatearMS(tiempo_ms){
         return (this+"").padStart(n, 0)
     }
     return H.ceros(2)+":"+M.ceros(2)+":"+S.ceros(2)+"."+MS.ceros(3)
+}
+
+//FUNCIONES DEL CUERPO HUMANO
+
+function mensajePrueba(){
+    alert("Has pulsado sobre el cuerpo humano");
 }
