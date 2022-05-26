24da227b4f655a8b624aef25d39df81b3795c089
JosemaPerez00 <77966853+JosemanuelPDR00@users.noreply.github.com>
2022-05-26 21:39:39 +0200

    Delete constantesConexionBBDD.php

--- Proyecto/templates/constantesConexionBBDD.php
+++
@@ -1,7 +0,0 @@
-<?php
-    define('DB_HOST', 'localhost');
-    define('DB_USUARIO', 'root');
-    define('DB_CONTRA', '123456');
-    define('DB_NOMBRE', 'bbddProyecto');
-    define('DB_CHARSET', 'utf8');
-?>
